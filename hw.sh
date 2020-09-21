952e26c7a65d7264e0bbb4542f209d3b918d45c7
root <root@localhost.localdomain>
2020-09-21 16:28:01 +0300

    copied and pasted, made it work

---
+++ hw.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
+cat <<'msgend'
+
+
+
+
+
+
+
+
+
+
+
+
+
+hello world
+this is my homework for linux
+privet ilya
+
+
+msgend
+sleep 5
+echo "continuing..."
+sleep 5
